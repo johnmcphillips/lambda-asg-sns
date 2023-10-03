0b802933e4f4b8c150c2d023bd5865897285fcfa
johnmcphillips <john.mcphillips@gmail.com>
2023-10-03 16:39:17 -0500

    Update lambda_function.py
    Added error handling, fixed logging order

--- lambda_function.py
+++ lambda_function.py
@@ -11,28 +11,36 @@ http = urllib3.PoolManager()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 def lambda_handler(event, context):
-    url = "https://hooks.slack.com/services/xxxx/xxxx/xxxx"
+    url = "https://hooks.slack.com/services/xxxxxx"
     headers = {
         "Content-Type": "application/json"
     }
-    
-    message = event['Records'][0]['Sns']['Message']
-    logger.info("Event: " + str(event))
-    logger.info("Message: " + str(message))
-    
-    message = ast.literal_eval(event['Records'][0]['Sns']['Message'])
-    reason = message['Cause']
-    
-    msg = json.dumps({
-        "channel": "#your-channel",
-        "username": "username",
-        "text": reason,
-        "icon_emoji": ":ghost:"
-    })
-    http.request('POST',url=url, body=msg, headers=headers)
-    
-    
 
+    try:
+        message = event['Records'][0]['Sns']['Message']
+        # Check if the message is a string containing 'null' and convert it to None
+        if message.strip().lower() == 'null':
+            message = None
+        else:
+            message = json.loads(message)  # Assuming the message is valid JSON
 
+        if message is not None:
+            reason = message['Cause']
+            msg = json.dumps({
+                "channel": "#channel",
+                "username": "username",
+                "text": reason,
+                "icon_emoji": ":ghost:"
+            })
+            http.request('POST', url=url, body=msg, headers=headers)
+            #print(reason)
+        else:
+            logger.warning("Received 'null' message, ignoring.")
+        
+        logger.info("Event: " + str(event))
+    
+    except KeyError as e:
+        logger.error(f"KeyError: {str(e)}")
+    except json.JSONDecodeError as e:
+        logger.error(f"JSONDecodeError: {str(e)}")
